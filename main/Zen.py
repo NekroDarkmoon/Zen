1818e8be0c7f651aa1a164e6e64133a6b98fd311
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-03-06 18:14:33 -0800

    Started work on utils

--- main/Zen.py
+++ main/Zen.py
@@ -1,9 +1,29 @@
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                         Imports
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Standard library imports
+import aiohttp
+import datetime
+import json
+import logging
+import sys
+import traceback
+
+# Third party imports
+import discord # noqa
+from discord.ext import commands as msg_commands
+from discord.app_commands import commands as slash_commands
+
+# Local application imports
+
+
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#                         Imports
+#                         Setup
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+description = "Hello, I'm a bot for TTRPG servers. I help with managing TTRPG communities."
+
+log = logging.getLogger()
+
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                         Imports
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
---
+++ main/cogs/utils/checks.py
@@ -0,0 +1,58 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                         Imports
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+from discord.ext import commands
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                       Async Checks
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+async def check_guild_permissions(ctx, perms, check=all):
+  """Checks guild wide permissions for a user.
+
+  Args:
+      ctx (_type_): _description_
+      perms (_type_): _description_
+      check (_type_, optional): _description_. Defaults to all.
+
+  Returns:
+      _type_: _description_
+  """
+  is_owner = await ctx.bot.is_owner(ctx.author)
+  if is_owner:
+    return True
+
+  if ctx.guild is None:
+    return False
+
+  resolved = ctx.author.guild_permissions
+  return check(getattr(resolved, name, None) == value for name, value in perms.items())
+
+
+async def check_permissions(ctx, perms, *, check=all) -> bool:
+  """Checks a user for certain permissions.
+
+  Args:
+      ctx (_type_): _description_
+      perms (_type_): _description_
+      check (_type_, optional): _description_. Defaults to all.
+
+  Returns:
+      bool: _description_
+  """
+  is_owner = await ctx.bot.is_owner(ctx.author)
+  if is_owner:
+    return True
+  
+  resolved = ctx.channel.permissions_for(ctx.author)
+  return check(getattr(resolved, name, None) == value for name, value in perms.items())
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                         Checks
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def has_permissions(*, check=all, **perms):
+  async def pred(ctx):
+    return await check_permissions(ctx, perms, check=check)
+  
+  return commands.check(pred)
