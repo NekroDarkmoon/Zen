5e397a9aa1319c4b28ad36157d21516aa8172e90
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-05-14 19:37:56 -0700

    Added semi complete get and xp msg event

--- main/cogs/xp.py
+++ main/cogs/xp.py
@@ -6,13 +6,14 @@ from datetime import datetime
 
 # Standard library imports
 import logging
+import re
 
-from itertools import zip_longest
 from typing import TYPE_CHECKING, Optional
 
 # Third party imports
 import discord  # noqa
 from async_lru import alru_cache
+from discord import app_commands
 from discord.ext import commands
 
 
@@ -47,6 +48,94 @@ class XP(commands.Cog):
     def __init__(self, bot: Zen) -> None:
         self.bot: Zen = bot
 
+    # --------------------------------------------------
+    #               App Commands Settings
+    xp_group = app_commands.Group(
+        name='xp', description='XP Commands.')
+
+    # ______________________ Give XP _______________________
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        # Validation
+        if re.search(r"^[^\"\'\.\w]", message.content) or message.author.bot:
+            return
+
+        # Check if xp enabled
+        if not self._get_xp_enabled(message.guild.id):
+            return
+
+        # Data builder
+        conn = self.bot.pool
+        guild = message.guild
+        author = message.author
+        time = message.created_at
+
+        try:
+            # TODO: Generate XP and data
+            # TODO: Update DB
+            pass
+        except Exception:
+            log.error("Error when giving rep on message.", exc_info=True)
+
+    # ________________________ Get XP _______________________
+    @xp_group.command(name='get')
+    @app_commands.describe(member='Gets the xp data for a user.')
+    async def display_xp(
+        self,
+        interaction: discord.Interaction,
+        member: Optional[discord.Member] = None
+    ) -> None:
+        # Defer
+        await interaction.response.defer()
+
+        # Validation
+        if not self._get_xp_enabled(interaction.guild_id):
+            return
+
+        member = member or interaction.user
+        conn = self.bot.pool
+
+        try:
+            # Get xp info
+            sql = ''' SELECT * FROM xp 
+                      INNER JOIN logger 
+                      ON 
+                        xp.server_id=logger.server_id 
+                      AND
+                        xp.user_id=logger.user_id
+                      WHERE server_id=$1 AND user_id=$2
+            '''
+            res = conn.execute(sql, interaction.guild_id, member.id)
+        except Exception:
+            log.error("Error while getting xp data.", exc_info=True)
+
+        # Build message
+        xp: int = res['xp'] if res['xp'] is not None else 0
+        level: int = res['level'] if res['level'] is not None else 0
+        next_level: int = None
+        needed_xp: int = None
+        num_msgs: int = res['msg_count'] if res['msg_count'] is not None else 0
+
+        msg = ''
+
+        e = discord.Embed(title=member.display_name,
+                          color=discord.Color.random())
+        e.description = msg
+        e.set_thumbnail(url=member.display_avatar.url)
+        e.add_field(name='XP', value=f'{xp}/{needed_xp}', inline=True)
+        e.add_field(name='Level', value=level, inline=True)
+        e.add_field(name='Messages', value=num_msgs, inline=True)
+
+        await interaction.edit_original_message(embed=e)
+
+    # _____________________ XP Enabled  _____________________
+    # _____________________ XP Enabled  _____________________
+    # _____________________ XP Enabled  _____________________
+    # _____________________ XP Enabled  _____________________
+    # _____________________ XP Enabled  _____________________
+    # _____________________ XP Enabled  _____________________
+    # _____________________ XP Enabled  _____________________
     # _____________________ XP Enabled  _____________________
 
     @alru_cache(maxsize=128)
