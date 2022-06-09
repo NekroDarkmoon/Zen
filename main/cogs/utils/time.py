e790fa4de0b8a915c1693964092510c7c81b5da2
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-06-08 19:03:31 -0700

    Added ShortTime

--- main/cogs/utils/time.py
+++ main/cogs/utils/time.py
@@ -5,11 +5,14 @@ from __future__ import annotations
 import datetime
 
 # Standard library imports
+import datetime
 import logging
-from typing_extensions import Self
-
 import parsedatetime as pdt
+import re
 
+
+from dateutil.relativedelta import relativedelta
+from typing_extensions import Self
 from typing import TYPE_CHECKING, Any, Optional
 
 # Third party imports
@@ -25,11 +28,42 @@ if TYPE_CHECKING:
 
 
 log = logging.getLogger(__name__)
+INVALID_TIME = 'Invalid time provided, try e.g. "tomorrow" or "3 days".'
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#                         Imports
+#                         Short Time
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class ShortTime:
+    compiled = re.compile(
+        """
+           (?:(?P<years>[0-9])(?:years?|y))?             # e.g. 2y
+           (?:(?P<months>[0-9]{1,2})(?:months?|mo))?     # e.g. 2months
+           (?:(?P<weeks>[0-9]{1,4})(?:weeks?|w))?        # e.g. 10w
+           (?:(?P<days>[0-9]{1,5})(?:days?|d))?          # e.g. 14d
+           (?:(?P<hours>[0-9]{1,5})(?:hours?|h))?        # e.g. 12h
+           (?:(?P<minutes>[0-9]{1,5})(?:minutes?|m))?    # e.g. 10m
+           (?:(?P<seconds>[0-9]{1,5})(?:seconds?|s))?    # e.g. 15s
+        """,
+        re.VERBOSE,
+    )
+
+    def __init__(
+        self, argument: str, *, now: Optional[datetime.datetime] = None
+    ) -> None:
+        match = self.compiled.fullmatch(argument)
+        if match is None or not match.group(0):
+            raise commands.BadArgument('invalid time provided.')
+
+        data = {k: int(v) for k, v in match.groupdict(default=0).items()}
+        now = now or datetime.datetime.utcnow()
+        self.dt: datetime.datetime = now + relativedelta(**data)
+
+        @classmethod
+        async def convert(cls, ctx: Context, argument: str) -> Self:
+            return cls(argument, now=ctx.message.created_at)
+
+
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                         Human Time
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
