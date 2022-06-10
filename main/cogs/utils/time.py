c3e799d6dd8d642a525fab7ca018080abe0914c5
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-06-09 20:31:33 -0700

    Added human timedelta

--- main/cogs/utils/time.py
+++ main/cogs/utils/time.py
@@ -270,6 +270,73 @@ class UserFriendlyTime:
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                         Imports
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def human_timedelta(
+    dt: datetime.datetime,
+    *,
+    source: Optional[datetime.datetime] = None,
+    accuracy: Optional[int] = 3,
+    brief: bool = False,
+    suffix: bool = True,
+) -> str:
+    now = source or datetime.datetime.now(datetime.timezone.utc)
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=datetime.timezone.utc)
+
+    if now.tzinfo is None:
+        now = now.replace(tzinfo=datetime.timezone.utc)
+
+    now = now.replace(microsecond=0)
+    dt = dt.replace(microsecond=0)
+
+    if dt > now:
+        delta = relativedelta(dt, now)
+        out_suffix = ''
+    else:
+        delta = relativedelta(now, dt)
+        out_suffix = ' ago' if suffix else ''
+
+    attrs = [
+        ('year', 'y'),
+        ('month', 'mo'),
+        ('day', 'd'),
+        ('hour', 'h'),
+        ('minute', 'm'),
+        ('second', 's'),
+    ]
+
+    output = list()
+    for attr, brief_attr in attrs:
+        elem = getattr(delta, attr + 's')
+        if not elem:
+            continue
+
+        if attr == 'day':
+            weeks = delta.weeks
+            if weeks:
+                if not brief:
+                    output.append(format(Plural(weeks), 'week'))
+                else:
+                    output.append(f'{weeks}w')
+
+        if elem <= 0:
+            continue
+
+        if brief:
+            output.append(f'{elem}{brief_attr}')
+        else:
+            output.append(format(Plural(elem), attr))
+
+    if accuracy is not None:
+        output = output[:accuracy]
+
+    if len(output) == 0:
+        return 'now'
+    else:
+        if not brief:
+            return human_join(output, final='and') + out_suffix
+        else:
+            return ' '.join(output) + out_suffix
+
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                         Imports
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
