f2ca61273fcc9432a270a83664ccff2a550d46d4
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-01-02 02:53:55 -0800

    Adds Sample config.py

---
+++ main/settings/sample_config.py
@@ -0,0 +1,13 @@
+client_id = ''
+token = ''
+guilds = []
+uri = 'postgresql://username:password.@localhost:5432/dbname'
+prefix = '~'
+activity = 'V4 - Prefix(&)'
+
+# a webhook to a channel for bot stats.
+stat_webhook = ('',
+                "")
+
+# when you generate your webhook, take the token and ID from the URL like so:
+# https://discord.com/api/webhooks/<id>/<token>
